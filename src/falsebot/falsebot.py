9e47dd33995a977d4cc69085944cd4a837c6e430
Fabricio <fabritomas25@gmail.com>
2023-07-16 19:43:46 -0300

    Welcome message with image

--- src/falsebot/falsebot.py
+++ src/falsebot/falsebot.py
@@ -1,5 +1,8 @@
 import discord
+from discord import File
 from discord.ext import commands
+from easy_pil import Editor, load_image_async
+from PIL import ImageFont
 
 class FalseBot(commands.Bot):
 
@@ -23,10 +26,37 @@ class FalseBot(commands.Bot):
         print(f'Logged as {self.user}')
 
     # New Member
-    #async def on_member_join(self, member):
+    # Generates a welcome embed message
+    async def on_member_join(self, member):
+        background_width = 800 # background image width
+        pfpsize = 240 # profile picture size (width = height)
+        pfpx = int(background_width / 2 - pfpsize / 2) # centered profile picture
+        pfpy = 20 # profile picture y position on background image
+        textx = int(background_width / 2) # centered texts
+        text1y = 280 # Title *
+        text2y = 370 # user mention **
 
+        channel = member.guild.system_channel
+        welcome_msg = f"BIENVENID@!" # Title *
+        welcome_mention = f"{member.global_name} ({member.name})" # user mention **
+
+        font = ImageFont.truetype(font='res/fonts/StoryElement.ttf', size=100)
+        font_small = ImageFont.truetype(font='res/fonts/StoryElement.ttf', size=40)
+
+        background = Editor("res/imgs/bg.jpg").blur()
+        pfp = await load_image_async(str(member.display_avatar.url))
+        profile = Editor(pfp).resize((pfpsize, pfpsize)).circle_image()
+
+        background.paste(profile, (pfpx, pfpy))
+        background.ellipse((pfpx, pfpy), pfpsize, pfpsize, outline="white", stroke_width=7)
+        background.text((textx, text1y), welcome_msg, color="white", font=font, align="center") # *
+        background.text((textx, text2y), welcome_mention, color="white", font=font_small, align="center") # **
+
+        msgfile = File(fp=background.image_bytes, filename="bg.jpg")
+        await channel.send(f"Bienvenid@ a {member.guild.name}! Esperamos que disfrutes mucho tu estancia.")
+        await channel.send(file=msgfile)
 
     # Bot read message
-    # ctx = context
-    async def on_message(ctx):
-        print("test-borrar")
+    async def on_message(self, message):
+        if(message.author == self):
+            return
