2a13a9d01e84e30c705d5f9e5d24e15ff779dc10
Fabricio <fabritomas25@gmail.com>
2023-07-16 22:42:40 -0300

    Added a user counter & changed layout

--- src/falsebot/falsebot.py
+++ src/falsebot/falsebot.py
@@ -29,28 +29,33 @@ class FalseBot(commands.Bot):
     # Generates a welcome embed message
     async def on_member_join(self, member):
         background_width = 800 # background image width
-        pfpsize = 240 # profile picture size (width = height)
+        pfpsize = 230 # profile picture size (width = height)
         pfpx = int(background_width / 2 - pfpsize / 2) # centered profile picture
         pfpy = 20 # profile picture y position on background image
         textx = int(background_width / 2) # centered texts
-        text1y = 280 # Title *
-        text2y = 370 # user mention **
+        text1y = 270 # Title *
+        text2y = 350 # user mention **
+        text3y = 405 # user count ***
 
         channel = member.guild.system_channel
         welcome_msg = f"BIENVENID@!" # Title *
         welcome_mention = f"{member.global_name} ({member.name})" # user mention **
+        num = sum(m.joined_at < member.joined_at for m in member.guild.members if m.joined_at is not None)
+        welcome_count = f"ERES EL MIEMBRO #{num}" # user count ***
 
         font = ImageFont.truetype(font='res/fonts/StoryElement.ttf', size=100)
         font_small = ImageFont.truetype(font='res/fonts/StoryElement.ttf', size=40)
+        font_count = ImageFont.truetype(font='res/fonts/StoryElement.ttf', size=25)
 
         background = Editor("res/imgs/bg.jpg").blur()
         pfp = await load_image_async(str(member.display_avatar.url))
         profile = Editor(pfp).resize((pfpsize, pfpsize)).circle_image()
 
         background.paste(profile, (pfpx, pfpy))
-        background.ellipse((pfpx, pfpy), pfpsize, pfpsize, outline="white", stroke_width=7)
+        background.ellipse((pfpx-5, pfpy-5), pfpsize+5, pfpsize+5, outline="white", stroke_width=7)
         background.text((textx, text1y), welcome_msg, color="white", font=font, align="center") # *
         background.text((textx, text2y), welcome_mention, color="white", font=font_small, align="center") # **
+        #background.text((textx, text3y), welcome_count, color="white", font=font_count, align="center") # ***
 
         msgfile = File(fp=background.image_bytes, filename="bg.jpg")
         await channel.send(f"Bienvenid@ a {member.guild.name}! Esperamos que disfrutes mucho tu estancia.")
